d454cb1f59e595290ce016628cfb8a89d9195af6
Aleksey Kukushkin <26064052+North-Earth@users.noreply.github.com>
2022-10-08 23:45:43 +0300

    Add request & response for slash commands

--- src/Shawa.Bot.Api/Controllers/SlashCommandsController.cs
+++ src/Shawa.Bot.Api/Controllers/SlashCommandsController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Shawa.Bot.Api.Models.Requests;
+using Shawa.Bot.Api.Models.Responses;
 
 namespace Shawa.Bot.Api.Controllers;
 
@@ -14,8 +16,37 @@ public class SlashCommandsController
     }
 
     [HttpPost("orders")]
-    public async Task<IActionResult> TakeOrder()
+    public async Task<ActionResult<SlashCommandResponse>> TakeOrder([FromForm] SlashCommandRequest request)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation(
+            "\n" +
+            "Command: {command}\n" +
+            "Text: {text}\n" +
+            "Token: {token}\n" +
+            "ChannelId: {channelId}\n" +
+            "ChannelMentions: {channelMentions}\n" +
+            "ChannelName: {channelName}\n" +
+            "ResponseUrl: {responseUrl}\n" +
+            "TeamId: {teamId}\n" +
+            "TriggerId: {triggerId}\n" +
+            "UserId: {userId}\n" +
+            "UserMentions: {userMentions}\n" +
+            "UserName: {userName}\n" +
+            "ChannelMentionsIds: {channelMentionsIds}\n" +
+            "UserMentionsIds: {userMentionsIds} \n", 
+            request.Command, request.Text, request.Token, 
+            request.ChannelId, string.Join(",", request.ChannelMentions ?? Array.Empty<string?>()), 
+            request.ChannelName, request.ResponseUrl, request.TeamId, request.TriggerId,
+            request.UserId, string.Join(",", request.UserMentions ?? Array.Empty<string?>()), 
+            request.UserName, string.Join(",", request.ChannelMentionsIds ?? Array.Empty<string?>()), 
+            string.Join(",", request.UserMentionsIds ?? Array.Empty<string?>()));
+
+        var response = new SlashCommandResponse
+        {
+            Text = "Hello Time :peepohey:",
+            ResponseType = "ephemeral",
+        };
+
+        return response;
     }
 }
