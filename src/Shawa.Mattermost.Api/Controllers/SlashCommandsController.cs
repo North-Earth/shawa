99e3e020d9ebc7d4342e5a89d4a0f32a9a40db68
Aleksey Kukushkin <26064052+North-Earth@users.noreply.github.com>
2022-10-11 23:09:23 +0300

    fix

--- src/Shawa.Mattermost.Api/Controllers/SlashCommandsController.cs
+++ src/Shawa.Mattermost.Api/Controllers/SlashCommandsController.cs
@@ -18,7 +18,86 @@ public class SlashCommandsController : ControllerBase
     {
         _logger = logger;
     }
+    
+    [HttpPost("friday")]
+    public async Task<ActionResult<SlashCommandResponse>> TakeOrder2([FromForm] SlashCommandRequest request)
+    {
+        _logger.LogInformation(
+            "Command: {command}\n" +
+            "Text: {text}\n" +
+            "Token: {token}\n" +
+            "ChannelId: {channelId}\n" +
+            "ChannelMentions: {channelMentions}\n" +
+            "ChannelName: {channelName}\n" +
+            "ResponseUrl: {responseUrl}\n" +
+            "TeamId: {teamId}\n" +
+            "TriggerId: {triggerId}\n" +
+            "UserId: {userId}\n" +
+            "UserMentions: {userMentions}\n" +
+            "UserName: {userName}\n" +
+            "ChannelMentionsIds: {channelMentionsIds}\n" +
+            "UserMentionsIds: {userMentionsIds} \n",
+            request.Command, request.Text, request.Token,
+            request.ChannelId, string.Join(",", request.ChannelMentions ?? Array.Empty<string?>()),
+            request.ChannelName, request.ResponseUrl, request.TeamId, request.TriggerId,
+            request.UserId, string.Join(",", request.UserMentions ?? Array.Empty<string?>()),
+            request.UserName, string.Join(",", request.ChannelMentionsIds ?? Array.Empty<string?>()),
+            string.Join(",", request.UserMentionsIds ?? Array.Empty<string?>()));
+        
+        if (_mattermostSettings.Value.Token != request.Token)
+        {
+            return Forbid();
+        }
 
+        var response = new SlashCommandResponse
+        {
+            Attachments = new []
+            {
+                new Attachment
+                {
+                    Text = "Hello Time!",
+                    Actions = new []
+                    {
+                        new Action
+                        {
+                            Id = "action_options",
+                            Name = "Select an option...",
+                            Integration = new ActionIntegration
+                            {
+                                Url = $"{_mattermostSettings.Value.Url}/friday/answer",
+                                Context = new ActionIntegrationContext()
+                                {
+                                    Action = "do_something"
+                                }
+                            },
+                            Type = "select",
+                            Options = new []
+                            {
+                                new Option
+                                {
+                                    Text = "Option1",
+                                    Value = "opt1"
+                                },
+                                new Option
+                                {
+                                    Text = "Option2",
+                                    Value = "opt2"
+                                },
+                                new Option
+                                {
+                                    Text = "Option3",
+                                    Value = "opt3"
+                                },
+                            },
+                        }
+                    }
+                }
+            }
+        };
+
+        return Ok(response);
+    }
+    
     [HttpGet("friday")]
     public async Task<ActionResult<SlashCommandResponse>> TakeOrder([FromForm] SlashCommandRequest request)
     {
@@ -98,10 +177,17 @@ public class SlashCommandsController : ControllerBase
         return Ok(response);
     }
     
+    [HttpGet("friday/answer")]
+    public IActionResult SendAnswer2()
+    {
+        _logger.LogInformation("Friday GET answer!");
+        return Ok();
+    }
+    
     [HttpPost("friday/answer")]
     public IActionResult SendAnswer()
     {
-        _logger.LogInformation("Friday answer!");
+        _logger.LogInformation("Friday POST answer!");
         return Ok();
     }
 }
